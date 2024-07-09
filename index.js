27eafa4489825ce876ade24432fa88058d90092e
Md. Kabirul Hossain <mdkabirulhossainj@gmail.com>
2024-07-09 12:20:01 +0600

    Completed  Get all the service data fromd mongodb database

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 
 //Middleware
@@ -33,6 +33,30 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
+    const userCollection = client.db('carGurdian').collection('services');
+
+    //find all data from mongodb
+    app.get('/services', async(req, res)=>{
+        const cursor = userCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+
+    app.get('/services/:id', async(req, res)=>{
+        const id = req.params.id;
+        const query ={_id: new ObjectId(id)};
+        const options = {
+            projection: { _id: 1, title: 1, img:1, price:1, service_id:1 },
+        };
+        const result = await userCollection.findOne(query, options);
+        res.send(result);
+    })
+
+    //Booking
+    app.post('/bookings', async(req, res)=>{
+      const booking= req.body;
+      console.log(booking);
+    })
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
